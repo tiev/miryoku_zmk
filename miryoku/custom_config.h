9c558c46a538d4c907ff61b36f25f7dad6a302c0
Viet Tran <phuocviet89@gmail.com>
2024-09-08 17:59:29 +0700

    update extra pinkie columns

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define U_WIN_TERM &kp LG(GRAVE)
+#define U_MAC_TERM &kp LG(LA(GRAVE))
+#define U_WIN_LANG &kp LG(SPACE)
